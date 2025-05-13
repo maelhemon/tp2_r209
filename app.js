8926e17da84ccea15848381d25f308d7b23b72fb
maelhemon <mael.hemon05@gmail.com>
2025-05-13 11:34:22 +0200

    html css vf7

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function shifoumi(){
     let test;
 }
+
+/* fonction choix_bot => générer un nombre aléatoire
+système de points => tests victoires plus incrémentation
+focntion shifumi avec eventlistener pour reset les points
+
+*/
